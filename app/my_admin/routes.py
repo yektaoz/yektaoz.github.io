915ef9b3547c15329421e0bc7fbb520b3741aef8
notyekta <yektaozcan94@gmail.com>
2024-05-06 16:32:52 +0300

    v0,1

--- app/my_admin/routes.py
+++
@@ -1,6 +0,0 @@
-from flask_admin import AdminIndexView, expose
-
-class MyAdminIndexView(AdminIndexView):
-  @expose("/")
-  def index(self):
-    return self.render("admin/my_admin_home_page.html")
--- app/product/__init__.py
+++
@@ -1,6 +0,0 @@
-# product/__init__.py
-from flask import Blueprint
-
-product = Blueprint('product', __name__)
-
-from . import routes
